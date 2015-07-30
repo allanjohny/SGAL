807d9f88e71309ba45631035eaabbc4848ef3a7e
allanjohny <allanjohny@gmail.com>
2015-07-30 10:48:41 -0400

    Commit inicial

---
+++ SGAL/SGAL.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SGAL.MVC.Startup))]
+namespace SGAL.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
