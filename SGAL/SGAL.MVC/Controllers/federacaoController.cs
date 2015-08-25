79cb209af6fab19dff0d91e5bb95831fbbc0ae02
allanjohny <allanjohny@gmail.com>
2015-08-24 20:56:46 -0400

    Criação do CRUD padrão e padrões novos de layout
    só sucesso agora!

---
+++ SGAL/SGAL.MVC/Controllers/federacaoController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using SGAL.Model.Logic.SGAL;
+using SGAL.Servicos.Logic;
+
+namespace SGAL.MVC.Controllers
+{
+    public class FederacaoController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View(new BaseService<federacao>().ObterTodos());
+        }
+
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+        public ActionResult Detalhar(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            return View(new BaseService<federacao>().Obter(id.Value));
+        }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Criar([Bind(Include = "descricao")] federacao federacao)
+        {
+            if (ModelState.IsValid)
+            {
+                //Campo atribuído automaticamente
+                federacao.datainclusao = federacao.dataalteracao = DateTime.Now;
+
+                var servico = new BaseService<federacao>();
+                servico.Incluir(federacao);
+                servico.Salvar();
+                return RedirectToAction("Index");
+            }
+
+            return View(federacao);
+        }
+
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            federacao federacao = new BaseService<federacao>().Obter(id.Value);
+            
+            if (federacao == null)
+                return HttpNotFound();
+            
+            return View(federacao);
+        }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar([Bind(Include = "federacaoid,descricao")] federacao federacao)
+        {
+            if (ModelState.IsValid)
+            {
+                //Campo atribuído automaticamente
+                federacao.dataalteracao = DateTime.Now;
+                var servico = new BaseService<federacao>();
+                servico.Alterar(federacao);
+                servico.Salvar();
+                return RedirectToAction("Index");
+            }
+            return View(federacao);
+        }
+
+        public ActionResult Excluir(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            federacao federacao = new BaseService<federacao>().Obter(id.Value);
+
+            if (federacao == null)
+                return HttpNotFound();
+            
+            return View(federacao);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmarExclusao(int id)
+        {
+            var servico = new BaseService<federacao>();
+            servico.Excluir(ent => ent.federacaoid == id);
+            servico.Salvar();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SGAL/SGAL.Model/Logic/SGAL/MetaData/federacao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SGAL.Model.Logic.SGAL
+{
+    [MetadataType(typeof(federacaoMetaData))]
+    public partial class federacao
+    {
+        class federacaoMetaData
+        {
+            [Required(ErrorMessage = "Campo Obrigatório")]
+            [DisplayName("Descrição")]
+            public string descricao { get; set; }
+
+            [DisplayName("Data de Inclusão")]
+            public DateTime? datainclusao { get; set; }
+
+            [DisplayName("Data de Alteração")]
+            public DateTime? dataalteracao { get; set; }
+        }
+    }
+}
